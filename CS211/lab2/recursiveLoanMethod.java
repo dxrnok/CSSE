faf2340d6fbcb1dfa5a4f189a020932477c2a8e9
dxrnok <konrad.sokal@gmail.com>
2024-03-22 12:29:09 +0000

    update

---
+++ CS211/lab2/recursiveLoanMethod.java
@@ -0,0 +1,31 @@
+package cs211;
+
+public class recursiveLoanMethod {
+	public static void main(String[] args) {
+		double x = loan(250000.0, 0.03, 1600.0);
+		String s = "months";
+		if(x == 12.0) {
+			x = x/12.0;
+			s = "year";
+		}else if (x > 12.0){
+		n	x = x/12.0;
+			s = "years";
+		}else if(x <= 1.0) {
+			s = "month";
+		}
+		
+		System.out.print("It will take ");
+		System.out.format("%.2f", x);
+		System.out.print(" "+ s + " to pay off the loan");
+	}
+	
+	public static double loan(double loan, double rate, double repay) {
+	    if (loan <= 0) {
+	        return 0;
+	    }
+	    
+	    double MI = rate/12;
+	    double CI = loan * (1 + MI);
+	    return 1+loan(CI - repay, rate, repay);
+	}
+}
---
+++ CS211/lab2/studentAttendanceMethod.java
@@ -0,0 +1,66 @@
+package cs211;
+import java.util.*;
+public class studentAttendanceMethod {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String attendance = "ppallpl".toUpperCase();
+		boolean result = checkIfFailed(attendance, 0, 0, 0);
+
+		//System.out.println("["+attendanceCheck(attendance)+" (NOT RECURSIVE)]");
+		
+		if(result) {
+			System.out.println("FAIL");
+		}else {
+			System.out.println("PASS");
+		}
+	}
+	
+	/*public static String attendanceCheck(String attendance) {
+		int countLate=0, countAbs=0;
+		String mark = "PASS";
+		
+		for(int i =0; i < attendance.length(); i++) {
+			if(attendance.charAt(i) == 'L') {
+				countLate++;
+			}else {
+				if(countLate < 3) {
+					countLate = 0;
+				}
+			}
+			
+			if(attendance.charAt(i) == 'A') {
+				countAbs++;
+			}
+		}
+		
+		if(countLate >=3 || countAbs >= 2) {
+			mark = "FAIL";
+		}
+		
+		return mark;
+	}*/
+	
+	public static boolean checkIfFailed(String attendance, int index, int cL, int cA) {
+		if(index < attendance.length()) {
+			if(attendance.charAt(index) == 'A') {
+				cA++;
+			}
+			
+			if(attendance.charAt(index) == 'L') {
+				cL++;
+			}else {
+				if(cL < 3) {
+					cL = 0;
+				}
+			}
+			
+			if(cA >= 2 || cL >= 3) {
+				return true;
+			}
+			
+			return checkIfFailed(attendance, index+1, cL, cA);
+		}else {
+			return false;
+		}
+	}
+}
