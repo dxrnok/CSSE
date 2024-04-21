1b501289b0a24ed3e937693d93f57303e15e6c06
dxrnok <konrad.sokal@gmail.com>
2024-04-22 00:15:01 +0100

    update

--- CS230/assignment-05-22429612/myApp.js
+++ CS230/assignment-05-22429612/myApp.js
@@ -3,6 +3,7 @@ const express = require('express');
 const connect = require("./connection/atlas_connect");
 const path = require('path');
 const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
