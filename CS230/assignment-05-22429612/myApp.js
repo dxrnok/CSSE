29c8215f979f51f48f80cf5e1f64f335ee84b060
dxrnok <konrad.sokal@gmail.com>
2024-04-22 01:25:57 +0100

    update CS230 05

--- CS230/assignment-05-22429612/myApp.js
+++ CS230/assignment-05-22429612/myApp.js
@@ -56,7 +56,7 @@ app.get('/createUser', (req, res) => {
 });
 
 app.get('/searchUser', (req, res) => {
-    res.render('userSearch');
+    res.render('searchUser');
 });
 
 app.get('/createUser/userShippingAddress', (req, res) => {
@@ -77,7 +77,7 @@ app.post('/createUser', async (req, res) => {
         };
 
         const userData = await collectionUser.insertOne(dataUser);
-        console.log("User Inserted Successfully:", userData);
+        console.log("User Data Inserted Successfully:", userData);
         console.log("Adding address...");
     
         userID = userData.insertedId;
@@ -120,7 +120,6 @@ app.post('/createUser', async (req, res) => {
 
 app.post('/createUser/userShippingAddress', async (req, res) => {
     try {
-        // Proceed with inserting the shipping address using userId
         const dataAddress = {
             customerID: userID,
             address1: req.body.address1,
@@ -131,7 +130,17 @@ app.post('/createUser/userShippingAddress', async (req, res) => {
         } 
         const shipData = await collectionShip.insertOne(dataAddress);
         console.log("User Data Inserted Successfully:", shipData);
-        res.send("User Created Successfully!");
+        //send a message and redirect to main page
+        const script = `
+            <script>
+                alert('User created successfully!');
+                window.location.href = '/';
+            </script>
+        `;
+        console.log("User created successfully!");
+
+        //Successful status and exec script
+        res.status(200).send(script);
     } catch (error) {
         console.error("Error Creating User: ", error);
         res.status(500).send("Error Creating User");
