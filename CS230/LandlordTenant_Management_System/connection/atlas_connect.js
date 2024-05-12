c39872d8fecf7b79d0c4bafdb7d80fa986b53d98
dxrnok <konrad.sokal@gmail.com>
2024-05-12 05:19:47 +0100

    update

--- CS230/LandlordTenant_Management_System/connection/atlas_connect.js
+++ CS230/LandlordTenant_Management_System/connection/atlas_connect.js
@@ -1,5 +1,5 @@
 module.exports = {
     database: {
-        url: "mongodb+srv://dbUser:dbUser-240@clusterdb.divzee2.mongodb.net/?retryWrites=true&w=majority&appName=ClusterDB"
+        url: "mongodb+srv://dbUser:dbUser-240@clusterdb.divzee2.mongodb.net/LandlordTenant?retryWrites=true&w=majority&appName=ClusterDB"
     }
 }
--- CS230/LandlordTenant_Management_System/myApp.js
+++ CS230/LandlordTenant_Management_System/myApp.js
@@ -5,10 +5,7 @@
 //*ADDITIONAL NOTES*
 //for any buttons containing "Random" no inputs are needed
 //
-
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
-const connect = require('./connection/atlas_connect');  //connection uri for atlas
 const app = express();
 
 //both below are middleware used for post methods
@@ -20,40 +17,6 @@ app.set('view engine', 'ejs');  //setting view engine for my dynmaic web
 app.use(express.static(__dirname + '/public')); //loading in middelware from /public
 //__dirname: path of the directory containing the file executed (i.e. myApp.js).
 
-//declaring vars for storing database information later
-var collectionTenant;
-var collectionLandlord;
-var collectionLandlordTenantContract;
-
-//creation of mongoclient with additional mongoclient options
-const client = new MongoClient(connect.database.url, {
-    serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
-    }
-});
-
-const dbName = 'LandlordTenant'; //declaring database connection
-//function to connect to MongoDB 
-async function connection() {
-    try {
-        //connect the client to the server
-        await client.connect();
-        console.log('Successfully Connected To MongoDB!');
-        const db = client.db(dbName);
-        collectionLandlord = db.collection('LandlordDetails');
-        collectionTenant = db.collection('TenantDetails');
-        collectionLandlordTenantContract = db.collection('LandlordTenantContract');
-        
-    } catch(error) {
-        // Ensures that the client will close when you finish/error
-        console.error(error);
-        client.close();
-    }
-}
-connection();
-
 //Main page for the website
 app.get('/', (req, res) => {
     res.render('index');
@@ -62,18 +25,27 @@ app.get('/', (req, res) => {
 const createRouter = require('./routes/create');  //router for (C) CRUD 
 app.use('/create', createRouter);
 
-//POST method for landlord creation
-app.post('/create/landlord', (req, res) =>{
-    try{
-        if(req.body.button === 'create'){
+const searchRouter = require('./routes/search');  //router for (R) CRUD 
+app.use('/search', searchRouter);
 
-        }
-    }catch(error){
-        console.error('Error Creating Member:', error);
-        res.status(500).send('Error Creating Member');
-    }
-});
+const updateRouter = require('./routes/update');  //router for (U) CRUD 
+app.use('/update', updateRouter);
+
+const deleteRouter = require('./routes/delete');  //router for (D) CRUD 
+app.use('/delete', deleteRouter);
 
 app.listen(8000, () => {
     console.log('Server Started On Port 8000');
 });
+
+//DATABASE
+//For my database structure i went for making separate documents for follwoing:
+//LandlordDetails, LandlordAddress, TenantDetails, TenantAddress, LandlordTenantContract.
+//I made to store common ids from ...Details to ...Address which allows easy finding each tenant/landlord data
+//This allowed me to avoid embbeding documents. Easier to navigate through isnt overwhealming
+
+//IMPACT ON CODE DEVELOPMENT
+//I made sure to create routes this time. This made it so much easier to read code and find simlpe errors
+//I had a major difficulty trying to find code errors in my past assignmnets, the routes way allowed me to 
+//develope a more clean and organised code. I added as much comments even to most basic parts just to make sure 
+//anyone can understand the code
